c91b8663572b63b12d702704fd9ec7055d8d17ab
Parth Gohil <gohilparth307@gmail.com>
2025-07-17 12:44:29 +0530

    Add files via upload

---
+++ A_Programe_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a= 0;
+    cout << "Enter a number: ";
+    cin >>n;
+    while (n>9||a>9) {
+        while(n>0) {
+            a+=n%10;
+            n/=10;
+        }
+        n=a;
+        a=0;
+    }
+    cout<<"Final: "<<n<< endl;
+}
